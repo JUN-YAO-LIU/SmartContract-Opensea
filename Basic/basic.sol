b269b74eb4557264e7b7d2f7413cc4b3c8c0b004
JUN-YAO-LIU <momomomomo7077@gmail.com>
2023-04-11 00:09:41 +0800

    variable home

--- Basic/basic.sol
+++ Basic/basic.sol
@@ -3,6 +3,17 @@ pragma solidity ^0.8.18;
 
 contract BasicSample{
 
+    // variable
+    uint test_default = 0;
+    uint8 test_uint8 = 0;
+    uint16 test_uint16 = 0;
+    uint32 test_uint32 = 0;
+    uint64 test_uint64 = 0;
+    uint128 test_uint128 = 0;
+    uint256 test_uint256 = 0; 
+
+    // typeof?
+    // convert
 
 }
 
