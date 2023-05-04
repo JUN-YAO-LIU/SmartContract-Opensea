81fa73577de93080928d1f1607a702ba5ccade6d
JUN-YAO-LIU <momomomomo7077@gmail.com>
2023-05-04 17:37:44 +0800

    library

--- Basic/LibraryTest.sol
+++ Basic/LibraryTest.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier:GPL-3.0
 pragma solidity ^0.8.19;
 
+// relevant-source-part-starts-here-and-spans-across-multiple-lines 這錯誤是因為沒有pure
 library TestLibrary{
     // library cannot have state variables.
     // uint u = 12;
-    function TestSum(uint[] calldata b) pure public{
+    function TestSum(uint[] calldata b) pure public returns(uint){
         uint[] calldata a = b;
-        a.length;
+        return a.length;
     }
 }
 
 contract AccessLibraryTest{
-    function TestSum(uint[] calldata a) public{
+    function TestSum(uint[] calldata a) pure public returns(uint b){
+      b = TestLibrary.TestSum(a);
     }
 }
